7000b25fed8e5f826bba50aa289c9281f6b1473e
Craig Manton <75326758+craigmantona@users.noreply.github.com>
2026-07-25 10:41:32 +0100

    Add Spotify client ID and redirect URI
    Added Spotify client ID and redirect URI to config.

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 window.SUPABASE_URL = "https://xevxjggauscyrvvtmftx.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_MJ-Iele0Fiei77xPx8YjtQ_oM6Lc7WW";
 window.LASTFM_API_KEY = "c88cf73114dbd066e2a1fd7af3f8296d";
+window.SPOTIFY_CLIENT_ID = "a7cb70fbe6a0427f8a2153414a9528d3";
+
+window.SPOTIFY_REDIRECT_URI =
+  "https://bank-of-music.pages.dev/";
